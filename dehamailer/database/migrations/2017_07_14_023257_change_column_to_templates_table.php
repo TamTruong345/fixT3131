ff8aa3a24c1363ea041661c6e433b93b20db4b65
trungnq <trungnq@deha-soft.com>
2017-07-14 09:41:13 +0700

    fix bug add template

---
+++ dehamailer/database/migrations/2017_07_14_023257_change_column_to_templates_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeColumnToTemplatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('templates', function (Blueprint $table) {
+            $table->longText('template_content')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
